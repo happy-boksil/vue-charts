a00f8894dcf84fc932da6ecf50a020f0486876e9
happy-boksil2 <happyBoksil2@gmail.com>
2021-04-26 23:40:22 +0900

    vue chart commit

---
+++ vue-charts/chart-lib/src/components/BarChart.vue
@@ -0,0 +1,31 @@
+<template>
+  <canvas ref="barChart" id="barChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+
+export default {
+    props: ['propsdata'],
+    mounted() {
+        var ctx = this.$refs.barChart;
+        new this.$_Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: this.propsdata
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-charts/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,41 @@
+<template>
+  <canvas ref="lineChart" id="lineChart"></canvas>
+</template>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};
+const config = {
+  type: 'line',
+  data,
+  options: {}
+};
+
+export default {
+    mounted() {
+        var myChart = new Chart(
+            this.$refs.lineChart,
+            config
+        );
+    }
+}
+</script>
+<style>
+
+</style>
